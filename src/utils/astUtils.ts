1e3563346f67303d6c30a1c34d0336cc13782262
konpeki622 <512054675@qq.com>
2021-08-04 16:40:46 +0800

    feat: optimize ast 'require.context' parser

--- src/utils/astUtils.ts
+++ src/utils/astUtils.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import { vueSfcAstParser } from '@originjs/vue-sfc-ast-parser';
 import { FileInfo, VueSFCContext } from '../ast-parse/astParse';
+import getParser from 'jscodeshift/src/getParser'
+import jscodeshift from 'jscodeshift'
 
 export function parseVueSfc (fileInfo: FileInfo) : VueSFCContext {
   if (!fileInfo.source || fileInfo.source.length === 0) {
@@ -17,4 +19,15 @@ export function parseVueSfc (fileInfo: FileInfo) : VueSFCContext {
   }
 
   return context
+}
+
+export function parseScriptSfc (fileInfo: FileInfo, lang?: string) : any {
+  if (!fileInfo.source || fileInfo.source.length === 0) {
+    fileInfo.source = fs.readFileSync(fileInfo.path).toString().split('\r\n').join('\n')
+  }
+  const parserOptions: string = lang || 'babylon'
+  const parser = getParser(parserOptions)
+  const jscodeshiftParser = jscodeshift.withParser(parser)
+  const scriptAST = jscodeshiftParser(fileInfo.source)
+  return scriptAST
 }
