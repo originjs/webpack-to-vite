e51260d835778cbb1a77e62dfdd7e7ddc096f7cf
yobrave <yobrave@outlook.com>
2021-12-28 20:19:07 +0800

    feat: print report sort by item.num (#77)
    * feat: printreport Sort by item.num
    
    * Update src/utils/report.ts
    
    Co-authored-by: Chieffo2021 <85914490+Chieffo2021@users.noreply.github.com>
    
    * Update src/utils/report.ts
    
    Co-authored-by: Joe Zhang <flyfish.zy@gmail.com>
    
    * Update src/utils/report.ts
    
    Co-authored-by: Joe Zhang <flyfish.zy@gmail.com>
    
    * Update tests/render.test.ts
    
    Co-authored-by: Chieffo2021 <85914490+Chieffo2021@users.noreply.github.com>
    
    * Update tests/render.test.ts
    
    Co-authored-by: Chieffo2021 <85914490+Chieffo2021@users.noreply.github.com>
    
    * Update src/utils/report.ts
    
    Co-authored-by: Chieffo2021 <85914490+Chieffo2021@users.noreply.github.com>
    
    * fix: render.test.ts error import
    
    * fix: Vars error
    
    Co-authored-by: yobrave <lizy@lanfun.com.cn>
    Co-authored-by: Chieffo2021 <85914490+Chieffo2021@users.noreply.github.com>
    Co-authored-by: Joe Zhang <flyfish.zy@gmail.com>

--- tests/render.test.ts
+++ tests/render.test.ts
@@ -2,6 +2,7 @@ import path from "path";
 import {TemplateData} from "../src/config/config";
 import {render} from "../src/generate/render";
 import {readSync} from "../src/utils/file";
+import { sortByNum } from "../src/utils/report"
 import fs from "fs";
 
 test('render vite.config.js from template',  () => {
@@ -23,3 +24,32 @@ test('render vite.config.js from template',  () => {
   expect(source).toMatch('viteCommonjs()')
   fs.rmdirSync(outDir, { recursive: true })
 });
+
+test('sort report by item.num', () => {
+  interface ConverObj {
+    num: string;// 'Number'
+    feat: string;// 'Conversion item'
+    times?: number;// 'Conversion times'
+  }
+  // test 1, normal
+  let reportList: ConverObj[] = []
+  const nums1 =  ["V06","B01","B04","V01"];
+  const result1 = ["B01","B04","V01","V06"];
+  reportList = nums1.map(num => ({
+    num,
+    feat: ''
+  }))
+  sortByNum(reportList)
+  expect(reportList.map(report => report.num)).toEqual(result1)
+
+  // test 2 empty
+  let reportList2: ConverObj[] = []
+  const nums2 =  [];
+  const result2 = [];
+  reportList2 = nums2.map(num => ({
+    num,
+    feat: ''
+  }))
+  sortByNum(reportList2)
+  expect(reportList2.map(report => report.num)).toEqual(result2)
+})
