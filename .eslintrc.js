2290be9d3d60ed94fc02dd73e93abb1f7769aa51
konpeki622 <512054675@qq.com>
2022-06-16 11:20:04 +0800

    feat: add lint check

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,37 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
   },
   extends: ['standard'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
   rules: {
     semi: 0,
     '@typescript-eslint/consistent-type-imports': [
       'error',
-      { prefer: 'type-imports' }
-    ]
-  }
+      { prefer: 'type-imports' },
+    ],
+    // js/ts
+    'eol-last': 'error',
+    'no-trailing-spaces': 'error',
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-multi-spaces': 'error',
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    camelcase: ['error', { properties: 'never' }],
+    'object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+  },
 }
