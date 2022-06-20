5eb9bdebec2eca968889b207a681dbb4b17fb334
KonpekiCode <512054675@qq.com>
2022-06-20 20:21:37 +0800

    feat: add lint check (#101)
    * feat: add lint check
    
    * fix: workflow
    
    * fix: add package.json

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -11,14 +11,14 @@ if (!releaseRE.test(msg) && !commitRE.test(msg)) {
   console.log()
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(
-      `invalid commit message format.`
+      'invalid commit message format.',
     )}\n\n` +
       chalk.red(
-        `  Proper commit message format is required for automated changelog generation. Examples:\n\n`
+        '  Proper commit message format is required for automated changelog generation. Examples:\n\n',
       ) +
-      `    ${chalk.green(`feat: add 'comments' option`)}\n` +
-      `    ${chalk.green(`fix: handle events on blur (close #28)`)}\n\n` +
-      chalk.red(`  See .github/commit-convention.md for more details.\n`)
+      `    ${chalk.green('feat: add \'comments\' option')}\n` +
+      `    ${chalk.green('fix: handle events on blur (close #28)')}\n\n` +
+      chalk.red('  See .github/commit-convention.md for more details.\n'),
   )
   process.exit(1)
 }
--- vitest.config.js
+++ vitest.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
   test: {
     root: 'tests',
     resolve: {
-      extensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+      extensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     },
     testTimeout: 2000,
-    globals: true
-  }
+    globals: true,
+  },
 })
